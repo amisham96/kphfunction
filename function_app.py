dbc151e116519135d292a1cb0ba022eb4a44247b
Amit Sharma <82436743+amisham96@users.noreply.github.com>
2025-03-28 16:29:28 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,22 +1,28 @@
+import datetime
 import logging
-import os
 
 import azure.functions as func
-from azure.storage.blob import BlobServiceClient
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobClient, BlobServiceClient, ContainerClient
 
-app= func.FunctionApp()
+app = func.FunctionApp()
 
-@app.blob_trigger(arg_name="myblob", path="container1/{name}.txt",
-                               connection="AzureWebJobsStorage") 
-def BlobTrigger(myblob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob"
-                f"Name: {myblob.name}"
-                f"Blob Size: {myblob.length} bytes")
+def upload_blob_data(blob_service_client: BlobServiceClient, container_name: str):
 
-    Connectionstring= os.getenv("AzureWebJobsStorage")
+     time_now  = datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S') 
+     blob_client = blob_service_client.get_blob_client(container=container_name, blob=f"{time_now}.txt")
+     data = b"Sample data for blob"
 
-    client = BlobServiceClient.from_connection_string(Connectionstring)
-    container_client = client.get_container_client("container1")
-    blob_client = container_client.get_blob_client(myblob.name)
-    blob_client.delete_blob()
-    logging.info(f"Blob {myblob.name} deleted successfully")
+     # Upload the blob data - default blob type is BlockBlob    
+     blob_client.upload_blob(data, blob_type="BlockBlob")
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
+def timer_trigger(myTimer: func.TimerRequest) -> None:
+
+     account_url = "https://kprg950f.blob.core.windows.net"
+     credential = DefaultAzureCredential()
+
+    # Create the BlobServiceClient object
+     blob_service_client = BlobServiceClient(account_url, credential=credential)
+     upload_blob_data(blob_service_client, "container1")
+     logging.info('Python timer trigger function executed.')
